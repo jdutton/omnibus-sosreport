0dd7b2394b0912c487fcb865ba06e392b5135341
Jeff Dutton <jeff.dutton@stratus.com>
2013-10-30 17:27:30 -0400

    Initial project created from 'omnibus project'

---
+++ config/projects/sosreport.rb
@@ -0,0 +1,21 @@
+
+name "sosreport"
+maintainer "CHANGE ME"
+homepage "CHANGEME.com"
+
+replaces        "sosreport"
+install_path    "/opt/sosreport"
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+# creates required build directories
+dependency "preparation"
+
+# sosreport dependencies/components
+# dependency "somedep"
+
+# version manifest file
+dependency "version-manifest"
+
+exclude "\.git*"
+exclude "bundler\/git"
